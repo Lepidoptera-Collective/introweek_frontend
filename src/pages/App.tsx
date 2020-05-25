be7a9a04d48c35bb2a8709de16b459f6a2143a8b
larstadema <larstadema@hotmail.com>
2020-05-26 00:26:18 +0200

    fix: switch to HashRouter for gh-pages support

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import PrivateRoute from 'components/PrivateRoute';
 
