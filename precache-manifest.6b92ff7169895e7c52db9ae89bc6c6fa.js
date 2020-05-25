3dd9d7f69cd1f65cebb5984e107838906257e363
larstadema <larstadema@hotmail.com>
2020-05-26 00:09:41 +0200

    Updates

---
+++ precache-manifest.6b92ff7169895e7c52db9ae89bc6c6fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c405f6e66c25110ff7b78124a631b8d2",
+    "url": "/introweek_frontend/index.html"
+  },
+  {
+    "revision": "55b5ee48536886d1aaeb",
+    "url": "/introweek_frontend/static/js/2.4e366583.chunk.js"
+  },
+  {
+    "revision": "8a9a2ae4a25f202f52818f8f8074424e",
+    "url": "/introweek_frontend/static/js/2.4e366583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa2f16607fead83f54a0",
+    "url": "/introweek_frontend/static/js/main.a40b71cd.chunk.js"
+  },
+  {
+    "revision": "d0bb557d2a074d2602c7",
+    "url": "/introweek_frontend/static/js/runtime-main.2bf71b0e.js"
+  },
+  {
+    "revision": "782612e8d660b8d81516f14dfe278cac",
+    "url": "/introweek_frontend/static/media/dummy_logo.782612e8.png"
+  }
+]);
