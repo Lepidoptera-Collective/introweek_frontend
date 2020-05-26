fbcad9afb3904652d8a8d05937bdea69cfc6f92e
larstadema <larstadema@hotmail.com>
2020-05-26 23:25:42 +0200

    feat: implemented twitch livestream

--- src/pages/Livestream.tsx
+++ src/pages/Livestream.tsx
@@ -1,21 +1,68 @@
 import React from 'react';
 
-import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import ReactPlayer from 'react-player';
+import TextField from '@material-ui/core/TextField';
 
 import Layout from 'layout/Layout';
+import useDebounce from 'utils/use-debounce';
+import useWindowSize from 'utils/use-window-size';
+import { DRAWER_WIDTH } from 'variables';
+import { useMediaQuery, useTheme } from '@material-ui/core';
 
 const LiveStream = () => {
+  const [channel, setChannel] = React.useState('');
+  const debouncedChannel = useDebounce(channel, 2000);
+  const windowSize = useWindowSize();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('xs'));
+  const [size, setSize] = React.useState({ height: 0, width: 0 });
+
+  React.useEffect(() => {
+    if (windowSize.width && windowSize.height) {
+      const pad = theme.spacing(3) * 2;
+      const drawerWidth = isMobile ? 0 : DRAWER_WIDTH;
+      const width = windowSize.width - drawerWidth - pad;
+      const height = width * (9 / 16);
+
+      setSize({
+        height,
+        width,
+      });
+    }
+  }, [isMobile, theme, windowSize]);
+
   return (
     <Layout>
-      <Container maxWidth="sm">
-        <Box my={4}>
+      <Box display="flex" flexDirection="column" my={4}>
+        <Box display="flex" justifyContent="space-between" mb={2}>
           <Typography variant="h4" component="h1" gutterBottom>
             LiveStream
           </Typography>
+          <TextField
+            color="secondary"
+            id="search-channels"
+            label="Channel name"
+            variant="outlined"
+            onChange={(event) => setChannel(event.target.value)}
+          />
         </Box>
-      </Container>
+        {debouncedChannel ? (
+          <Box alignSelf="center" mt={3}>
+            <ReactPlayer
+              url={`https://www.twitch.tv/${debouncedChannel}`}
+              muted
+              playing
+              {...size}
+            />
+          </Box>
+        ) : (
+          <Typography variant="body1" align="center">
+            Vul een valide kanaal naam in.
+          </Typography>
+        )}
+      </Box>
     </Layout>
   );
 };
