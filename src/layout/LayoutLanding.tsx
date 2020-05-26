fc53154d51f6ba6105a8e2facf94411804bac415
larstadema <larstadema@hotmail.com>
2020-05-26 17:38:19 +0200

    fix: landing pages styling mobile

--- src/layout/LayoutLanding.tsx
+++ src/layout/LayoutLanding.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import { AppBar, Box } from '@material-ui/core';
@@ -30,35 +28,28 @@ const useStyles = makeStyles((theme: Theme) =>
 const LayoutLanding: React.FC = ({ children }) => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('xs'));
-  const [mobileOpen, setMobileOpen] = React.useState(false);
   const classes = useStyles();
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
 
   return (
     <div className={classes.root}>
       {isMobile && (
         <AppBar position="fixed" color="primary">
           <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open menu"
-              edge="start"
-              onClick={handleDrawerToggle}
-              className={classes.menuButton}
-            >
-              <MenuIcon />
-            </IconButton>
             <Typography variant="h6" noWrap>
               Introweek
             </Typography>
           </Toolbar>
         </AppBar>
       )}
-      <Box display="flex" flexDirection="row" height="100%" width="100%">
-        <Box flex={1} display="flex" flexDirection="row" justifyContent="center">
+      <Box display="flex" flexDirection={isMobile ? 'column' : 'row'} height="100%" width="100%">
+        {isMobile && <div className={classes.toolbar} />}
+        <Box
+          flex={1}
+          display="flex"
+          flexDirection="row"
+          justifyContent="center"
+          maxHeight={isMobile ? '100px' : undefined}
+        >
           <div style={{ backgroundColor: '#662d92', height: '100%', flex: 1 / 6 }} />
           <div style={{ backgroundColor: '#f25927', height: '100%', flex: 1 / 6 }} />
           <div style={{ backgroundColor: '#20ade3', height: '100%', flex: 1 / 6 }} />
@@ -66,7 +57,12 @@ const LayoutLanding: React.FC = ({ children }) => {
           <div style={{ backgroundColor: '#17a945', height: '100%', flex: 1 / 6 }} />
           <div style={{ backgroundColor: '#ed1922', height: '100%', flex: 1 / 6 }} />
           <img
-            style={{ width: '300px', height: '300px', position: 'absolute', alignSelf: 'center' }}
+            style={{
+              width: isMobile ? '100px' : '300px',
+              height: isMobile ? '100px' : '300px',
+              position: 'absolute',
+              alignSelf: 'center',
+            }}
             src={logo}
             alt="introweek logo"
           />
